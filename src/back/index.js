227add34c40914785750a5111d41bf8888292b52
matisosagit <matiassosa1314@gmail.com>
2025-06-07 01:52:02 -0300

    correccion de index

--- src/back/index.js
+++ src/back/index.js
@@ -5,6 +5,17 @@ import sesion from './sesion.js';
 import rutasUsuario from './usuarios.js';
 import router from './clientes.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+// Esto te da la ruta real de este archivo (index.js)
+const __filename = fileURLToPath(import.meta.url);
+
+// Esto te da la carpeta donde está el archivo (igual que __dirname en CommonJS)
+const __dirname = dirname(__filename);
+
+
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({ extended: true }));
