90262a00bd3e75200f0ff90631f5392f655087f6
matisosagit <matiassosa1314@gmail.com>
2025-06-07 02:50:44 -0300

    cambios en el comando build y en el ruteo

--- src/back/index.js
+++ src/back/index.js
@@ -17,13 +17,13 @@ const __dirname = dirname(__filename);
 
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../../build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(sesion);  
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
