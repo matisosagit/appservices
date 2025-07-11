fb553f55bb6f10a8b62c3cd0c2a2f9221ac2d710
matisosagit <matiassosa1314@gmail.com>
2025-07-11 18:27:20 -0300

    archivo para repuestos- control de stock js

--- src/back/productos.js
+++ src/back/productos.js
@@ -30,3 +30,46 @@ let Repuesto;
         tableName: 'repuestos'
     });
 })();
+
+router.post('anadir-repuesto', async (req, res) => {
+    const {nombre, cantidad} = req.body;
+    const userId = req.session.usuarioId;
+    try{
+        const repuesto = await Repuesto.create(
+            {
+                usuario_id: userId,
+                nombre: nombre,
+                cantidad: cantidad
+            },
+        );
+        res.status(201).json({message:'Repuesto anadido correctamente',repuesto});
+    }catch(e){
+        console.error(e);
+        res.status(500).json({message: 'Error al anadir repuesto', e})
+    }
+});
+
+router.put('editar-repuesto', async(req,res) => {
+    const cant = req.body;
+    const id = req.params;
+    try{
+        const rep_act = await Repuesto.update(
+            {cantidad: cant}, {where: {id: id}}
+        );
+        res.status(201).json({message:'OK', rep_act});
+    }catch(e){
+        console.error(e);
+        res.status(500).json({message:'Error al editar datos', e});
+    }
+});
+
+router.get('lista-stock', async(req, res) => {
+    const user_id = req.session.usuarioId;
+    try{
+        const lista_stock = await Repuesto.findAll({where:{usuario_id: user_id}});
+        res.status(201).json(lista_stock);
+    }catch(e){
+        console.error(e);
+        res.status(500).json({message: 'Error al buscar lista de stock', e});
+    }
+});
