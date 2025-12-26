ed8761c3685dd03d105850a4154137683abee797
matisosagit <matiassosa1314@gmail.com>
2025-12-26 20:45:51 -0300

    index

--- back/index.js
+++ back/index.js
@@ -1,31 +1,23 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
 
 import conectarBD from "./conexion.js";
 import sesion from "./sesion.js";
 import rutasUsuario from "./usuarios.js";
 import router from "./clientes.js";
 
-
-
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); 
+app.set("trust proxy", 1);
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(sesion);
 
 app.use("/api/usuarios", rutasUsuario);
 app.use("/api/clientes", router);
 
-app.use(express.static(path.join(__dirname, "../front/build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../front/build", "index.html"));
-}); 
 
 (async () => {
   try {
@@ -33,14 +25,12 @@ app.get("*", (req, res) => {
     await sequelize.sync({ force: false });
 
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`Servidor escuchando en http://localhost:${PORT}`);
-    });
+    app.listen(PORT, () =>
+      console.log(`Servidor escuchando en puerto ${PORT}`)
+    );
   } catch (error) {
     console.error("Error al iniciar el servidor:", error);
   }
 })();
-
-
 
 
