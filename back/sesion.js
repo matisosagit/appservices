2e3527827451b902210c3a032f7ad88b0244cdf6
matisosagit <matiassosa1314@gmail.com>
2025-12-28 01:24:11 -0300

    session samesite

--- back/sesion.js
+++ back/sesion.js
@@ -7,7 +7,7 @@ const sesion = session({
   cookie: {
     secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax",
+    sameSite: "lax",
     maxAge: 24 * 60 * 60 * 1000
   }
 });
