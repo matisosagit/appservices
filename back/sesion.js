5c90ffc28a3dbf9e42f5fbcdd102719eb45c6a45
matisosagit <matiassosa1314@gmail.com>
2025-12-28 02:22:01 -0300

    name

--- back/sesion.js
+++ back/sesion.js
@@ -1,11 +1,12 @@
 import session from 'express-session';
 
 const sesion = session({
+  name: "sid",
   secret: process.env.SESSION_SECRET || 'tu_secreto_aqui',
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     httpOnly: true,
     sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
