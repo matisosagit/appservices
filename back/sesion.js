adc3da8f537f1f489efc822758536e720b5bffaa
matisosagit <matiassosa1314@gmail.com>
2025-12-28 03:07:03 -0300

    problema cookies, vuelvo al paso anterior sin pg

--- back/sesion.js
+++ back/sesion.js
@@ -1,31 +1,18 @@
 import session from 'express-session';
-import pgSession from 'connect-pg-simple';
-import pg from 'pg';
-
-const PgSession = pgSession(session);
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false
-});
 
 const sesion = session({
-  store: new PgSession({
-    pool,
-    tableName: 'session'
-  }),
+  name: "sid",
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === 'production',
+    secure: true,
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000
   }
 });
 
 export default sesion;
+
 
