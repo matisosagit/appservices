c455f4ef00c274ea03e941ab4c54c9bf665dd817
matisosagit <matiassosa1314@gmail.com>
2026-03-12 20:22:36 -0300

    corrijo error al importar las funciones del controlador de usuarios

--- back/rutas/usuarios.js
+++ back/rutas/usuarios.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import bcrypt from "bcryptjs";
 import Usuario from '../modelos/usuario.js';
-import user from '../controladores/user.Controllers.js';
+import * as user from '../controladores/user.Controllers.js';
 
 const router = Router();
 
